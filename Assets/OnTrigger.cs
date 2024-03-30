c041ceb4291496edae28ce647f5fddd9fde239fa
Alfredo <141881587+Juuicee@users.noreply.github.com>
2024-03-30 00:44:01 -0400

    Configured Smoke and Fire Particle
    Made it so that the particle effect only happens when the coal object has collision with the furnace, also messed around with some of the particle effects to make them look more like fire and smoke

--- Assets/OnTrigger.cs
+++ Assets/OnTrigger.cs
@@ -2,23 +2,62 @@ using UnityEngine;
 
 public class Coal : MonoBehaviour
 {
-    private bool isBurning = false; // Flag to track if the furnace is burning
+    public Animation burningAnimation; // Reference to the burning animation
+    public ParticleSystem smokeParticles; // Reference to the smoke particle system
+    public ParticleSystem fireParticles; // Reference to the fire particle system
+
+    private bool isBurning = false; // Flag to track if the coal is burning
 
     private void OnTriggerEnter(Collider other)
     {
-        Furnace furnace = other.GetComponent<Furnace>(); 
-        if (furnace != null)
+        if (!isBurning)
         {
-            furnace.SetCoalReference(this); // Pass a reference to the coal object
-            furnace.StartBurning();
-            Debug.Log("Coal added to furnace.");
-            isBurning = true; 
+            Furnace furnace = other.GetComponent<Furnace>(); 
+            if (furnace != null)
+            {
+                furnace.SetCoalReference(this); // Pass a reference to the coal object
+                furnace.StartBurning();
+                Debug.Log("Coal added to furnace.");
+                isBurning = true; 
+
+                // Start the burning animation when the coal enters the furnace
+                if (burningAnimation != null)
+                {
+                    burningAnimation.Play();
+                }
+
+                // Activate the particle systems for fire and smoke
+                if (smokeParticles != null)
+                {
+                    smokeParticles.Play();
+                }
+                if (fireParticles != null)
+                {
+                    fireParticles.Play();
+                }
+            }
         }
     }
 
     // Called when the furnace finishes burning
     public void OnBurningComplete()
     {
+        // Stop the particle systems for fire and smoke
+        if (smokeParticles != null)
+        {
+            smokeParticles.Stop();
+        }
+        if (fireParticles != null)
+        {
+            fireParticles.Stop();
+        }
+
+        // Stop the burning animation
+        if (burningAnimation != null)
+        {
+            burningAnimation.Stop();
+        }
+
         // Destroy the coal object
         Destroy(gameObject);
     }
@@ -27,13 +66,22 @@ public class Coal : MonoBehaviour
     {
         if (isBurning)
         {
-            // If the coal is burning, don't destroy it immediately
-            return;
+            // If the coal is burning, stop the particle systems and animation when it exits the furnace
+            if (smokeParticles != null)
+            {
+                smokeParticles.Stop();
+            }
+            if (fireParticles != null)
+            {
+                fireParticles.Stop();
+            }
+            if (burningAnimation != null)
+            {
+                burningAnimation.Stop();
+            }
         }
 
         // If the coal is not burning anymore and it exits the furnace collider, destroy it
         Destroy(gameObject);
     }
 }
-
-
--- Assets/~Imported/SimpleLowPolyNature/Shaders/BurningEffectShader.shader
+++ Assets/~Imported/SimpleLowPolyNature/Shaders/BurningEffectShader.shader
@@ -2,35 +2,53 @@ Shader "Custom/BurningEffectShader"
 {
     Properties
     {
-        _Color ("Color", Color) = (1,1,1,1)
-        _EmissionColor ("Emission Color", Color) = (1,1,1,1)
-        _EmissionIntensity ("Emission Intensity", Range(0, 1)) = 0
+        _MainTex ("Texture", 2D) = "white" {} // Main texture
+        _BurnColor ("Burn Color", Color) = (1, 0.5, 0, 1) // Color when burning
+        _BurnIntensity ("Burn Intensity", Range(0, 1)) = 0 // Intensity of burning effect
+        _GlowColor ("Glow Color", Color) = (1, 0.5, 0, 1) // Color of glow
+        _GlowIntensity ("Glow Intensity", Range(0, 1)) = 0 // Intensity of glow effect
     }
     SubShader
     {
         Tags { "RenderType"="Opaque" }
-        LOD 200
-
+        LOD 100
+        
         CGPROGRAM
         #pragma surface surf Standard fullforwardshadows
-        #pragma target 3.0
 
+        // Shader properties
         sampler2D _MainTex;
-        fixed4 _Color;
-        fixed4 _EmissionColor;
-        half _EmissionIntensity;
+        fixed4 _BurnColor;
+        float _BurnIntensity;
+        fixed4 _GlowColor;
+        float _GlowIntensity;
 
         struct Input
         {
             float2 uv_MainTex;
         };
 
+        // Surface shader function
         void surf (Input IN, inout SurfaceOutputStandard o)
         {
-            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
-            o.Albedo = c.rgb;
-            o.Emission = _EmissionColor.rgb * _EmissionIntensity;
-            o.Alpha = c.a;
+            // Sample the main texture
+            fixed4 mainTexColor = tex2D(_MainTex, IN.uv_MainTex);
+
+            // Calculate the final color based on burn intensity
+            fixed4 finalColor = lerp(mainTexColor, _BurnColor, _BurnIntensity);
+
+            // Apply emission for burning effect with increased glow intensity
+            o.Emission = finalColor.rgb * _BurnIntensity * (_GlowIntensity * 2); // Adjusted for increased glow intensity
+
+            // Apply final color
+            o.Albedo = finalColor.rgb;
+
+            // Apply smoothness and metallic from main texture
+            o.Smoothness = mainTexColor.a;
+            o.Metallic = mainTexColor.r;
+
+            // Apply alpha
+            o.Alpha = mainTexColor.a;
         }
         ENDCG
     }
