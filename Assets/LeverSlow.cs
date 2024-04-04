061cd76dd5b0e93c47aca67446862c20e56b54d3
Kerene Wright <94418070+KLAW4life@users.noreply.github.com>
2024-04-04 18:32:13 -0400

    Finished Break

--- Assets/LeverSlow.cs
+++ Assets/LeverSlow.cs
@@ -2,27 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WhistleSpeed : MonoBehaviour
+public class LevelSlow : MonoBehaviour
 {
 
     public rotation rotScript;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnTriggerStay(Collider other)
     {
-        rotScript.SpeedUpRotation();
+        rotScript.SlowDownRotation();
     }
 }
 
