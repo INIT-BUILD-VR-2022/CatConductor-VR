2393648a86394aad6fdc7364fcd757c88a6db763
Christian Deangelo Orozco <chrisorozco305@gmail.com>
2024-04-05 17:37:18 -0400

    Made changes to choochooSound Script

--- Assets/ChooChooTrigger.cs
+++ Assets/ChooChooTrigger.cs
@@ -9,18 +9,64 @@ public class ChooChooTrigger : MonoBehaviour
     public Transform speedTriggerTransform;
 
     private bool isWithinSpeedTrigger = false;
+    private bool isLooping = false;
+    public float loopStart = 0.3f;
+    public float loopEnd = 0.4f;
 
     // Update is called once per frame
     void Update()
     {
-        if (speedTriggerTransform != null && audioSource != null && chooChooSound != null)
+        isWithinSpeedTrigger = speedTriggerTransform.GetComponent<Collider>().bounds.Contains(transform.position);
+
+        // If the lever is within the speed trigger
+        if (isWithinSpeedTrigger)
         {
-            isWithinSpeedTrigger = speedTriggerTransform.GetComponent<Collider>().bounds.Contains(transform.position);
-            
-            if (isWithinSpeedTrigger && !audioSource.isPlaying)
+            if (!isLooping)
             {
-                audioSource.PlayOneShot(chooChooSound);
+                StartLoop();
             }
-        }        
+        }
+        else
+        {
+            if (isLooping)
+            {
+                StopLoop();
+            }
+        }
+    }
+
+    private void StartLoop()
+    {
+        if (audioSource != null && chooChooSound != null)
+        {
+            audioSource.clip = chooChooSound;
+            audioSource.time = loopStart;
+            audioSource.Play();
+            audioSource.loop = true;
+            isLooping = true;
+
+            // Schedule the end of the loop
+            Invoke("LoopSection", loopEnd - loopStart);
+        }
+    }
+
+    private void LoopSection()
+    {
+        if (isWithinSpeedTrigger && isLooping)
+        {
+            // Restart from loop point
+            audioSource.time = loopStart;
+            Invoke("LoopSection", loopEnd - loopStart);
+        }
+    }
+
+    private void StopLoop()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        isLooping = false;
+        CancelInvoke("LoopSection");
     }
 }
