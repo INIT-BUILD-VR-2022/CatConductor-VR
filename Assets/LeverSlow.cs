00f7772535fb9a133e827d267c3f1f7f192e3f59
Kerene Wright <94418070+KLAW4life@users.noreply.github.com>
2024-04-06 02:43:37 -0400

    added sparks

--- Assets/LeverSlow.cs
+++ Assets/LeverSlow.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using UnityEngine.ParticleSystemModule;
 
 public class LevelSlow : MonoBehaviour
 {
-    Vector3 ImpulseVector = new Vector3(0.0f, 5000.0f, 10000.0f);
-    Vector3 spinVector = new Vector3(50000.0f, 50000.0f, 50000.0f);
+
     public rotation rotScript;
+    //public Rotation rotScript; // Assuming Rotation is the correct class 
 
+    public ParticleSystem brakeSparks;
 
-    // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("Lever Script");
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
 
@@ -24,31 +25,11 @@ public class LevelSlow : MonoBehaviour
     void OnTriggerStay(Collider other)
     {
         rotScript.SlowDownRotation();
-        if (other.gameObject.tag == "HitBox")
-        {
-            StartCoroutine(Collide());
-        }
+
+        brakeSparks.Play();
     }
 
-    IEnumerator Collide()
-    {
-
-
-        Time.timeScale = 0;
-
-        yield return new WaitForSecondsRealtime(rotScript.rotationSpeed * .25f);
-
-        Time.timeScale = 1;
-
-        transform.parent = null;
-        GetComponent<Rigidbody>().AddForce(ImpulseVector, ForceMode.Force);
-        GetComponent<Rigidbody>().AddTorque(spinVector, ForceMode.Force);
-        GetComponent<Rigidbody>().useGravity = true;
-        GetComponent<Collider>().enabled = false;
-
-        yield return new WaitForSecondsRealtime(3f);
-        Destroy(gameObject);
-    }
+  
 }
 
 
