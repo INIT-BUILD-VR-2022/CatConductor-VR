57bc70acdde94c013a3d76f0b39ac86c2311d4e5
LyHuuThang <94418070+KLAW4life@users.noreply.github.com>
2024-04-06 00:34:55 -0400

    Test sparks

--- Assets/LeverSlow.cs
+++ Assets/LeverSlow.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class LevelSlow : MonoBehaviour
 {
-
+    Vector3 ImpulseVector = new Vector3(0.0f, 5000.0f, 10000.0f);
+    Vector3 spinVector = new Vector3(50000.0f, 50000.0f, 50000.0f);
     public rotation rotScript;
 
 
@@ -23,6 +24,31 @@ public class LevelSlow : MonoBehaviour
     void OnTriggerStay(Collider other)
     {
         rotScript.SlowDownRotation();
+        if (other.gameObject.tag == "HitBox")
+        {
+            StartCoroutine(Collide());
+        }
+    }
+
+    IEnumerator Collide()
+    {
+
+
+        Time.timeScale = 0;
+
+        yield return new WaitForSecondsRealtime(rotScript.rotationSpeed * .25f);
+
+        Time.timeScale = 1;
+
+        transform.parent = null;
+        GetComponent<Rigidbody>().AddForce(ImpulseVector, ForceMode.Force);
+        GetComponent<Rigidbody>().AddTorque(spinVector, ForceMode.Force);
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Collider>().enabled = false;
+
+        yield return new WaitForSecondsRealtime(3f);
+        Destroy(gameObject);
     }
 }
+
 
