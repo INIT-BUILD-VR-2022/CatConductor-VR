eee72d10ddf2f9c85aa7bdd9baf58e7caac0f774
Felix Jarquin <fjarq005@fiu.edu>
2024-04-15 13:52:16 -0400

    Final Push for Beta

--- Assets/ButtonManager.cs
+++ Assets/ButtonManager.cs
@@ -7,7 +7,7 @@ public class ButtonManager : MonoBehaviour
     public void quitButton()
     {
         Debug.Log("Bro is leaving the game!! ");
-        UnityEditor.EditorApplication.isPlaying = false; // Should stop the Editor when pressed now too
+        
         Application.Quit();//Stop the normies (actual players' game)
     }
 }
--- Assets/MainMenuManager.cs
+++ Assets/MainMenuManager.cs
@@ -8,7 +8,7 @@ public class MainMenuManager : MonoBehaviour
     public void quitButton()
     {
         Debug.Log("Bro is leaving the game!! ");
-        UnityEditor.EditorApplication.isPlaying = false; // Should stop the Editor when pressed now too
+       
         Application.Quit();//Stop the normies (actual players' game)
     }
 
