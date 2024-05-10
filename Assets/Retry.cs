c20f5e1199fd0f41bc871641576395c47a368f14
CrisBlu <55038118+CrisBlu@users.noreply.github.com>
2024-05-10 12:03:42 -0400

    Gameover fixed

--- Assets/Retry.cs
+++ Assets/Retry.cs
@@ -7,6 +7,6 @@ public class Retry : MonoBehaviour
 {
     public void ReloadScene()
     {
-        SceneManager.LoadScene("Main Game");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
